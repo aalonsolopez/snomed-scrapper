04e0109d3b6989ec553e1d45418afdbd8dd5aa78
Alejandro Alonso López <aalonsopersonal@gmail.com>
2024-01-06 23:10:04 +0100

    Add JSDocs to NotValidFHIRResourceError class

--- src/errors/NotValidFHIRResourceError.js
+++ src/errors/NotValidFHIRResourceError.js
@@ -1,4 +1,13 @@
+/**
+ * Indicates that the resource is not a valid FHIR resource
+ * @class NotValidFHIRResourceError
+ * @extends {Error}
+ */
 export default class NotValidFHIRResourceError extends Error {
+    /**
+     * Creates an instance of NotValidFHIRResourceError.
+     * @param {string} message - The error message.
+     */
     constructor(message) {
         super(message);
         this.name = 'NotValidFHIRResourceError';
